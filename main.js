a19fe477e4fe26ff46f52d2cce81314cdf1477c6
annarood1 <anna.rood@edu.sait.ca>
2024-11-17 19:26:24 -0700

    Add footer javascript

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ function greeting() {
         message = 'GET YOUR BUTT TO BED! &#127772;';
         style = 'night';
     } else if (hour <= 11) {
-        message = 'Good morning!';
+        message = 'Good morning! &#127774;';
         style = 'morning';
     } else if (hour <= 17) {
-        message = 'Good afternoon!';
+        message = 'Good afternoon! $#127763;';
         style = 'afternoon';
     } else {
-        message = 'Good evening!';
+        message = 'Good evening! $#127768;';
         style = 'evening';
     }
 
@@ -29,6 +29,15 @@ function greeting() {
     greetingText.innerHTML = message;
     greetingBox.classList.add(style);
 }
+
+const footerCC = document.getElementById("copyright");
+
+year = currentDate.getFullYear();
+console.log(year);
+footerCC.innerHTML = "&copy; Anna Rood, " + year + "<span id='right'>BY-NC-SA 4.0</span>";
+
+console.log(currentDate);
+
 // Wait until after the document has loaded to run greeting
 document.addEventListener('DOMContentLoaded', () => {
         greeting();
